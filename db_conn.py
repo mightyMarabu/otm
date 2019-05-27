9b2bd1356598b32b9fe5cf0076440a8bc1d0244a
mightyMarabu <sebastian.n.schmidt@gmail.com>
2019-05-27 21:06:21 +0200

    portnr

--- db_conn.py
+++ db_conn.py
@@ -2,7 +2,7 @@
 import psycopg2
 
 #try:
-#    conn = psycopg2.connect(dbname="postgres", user="postgres", host="192.168.70.134", port="54322", password="postgres")
+#    conn = psycopg2.connect(dbname="postgres", user="postgres", host="192.168.70.134", port="9876", password="postgres")
 #    print(conn)
 #except Exception as e:
 #    print ("\n no f**kin' connection! \n what else can i say?")
